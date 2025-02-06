f555c6b187434229d3b4b31df6070ed25e24debb
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-02-06 18:07:42 -0300

    Creating Routing and index

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { StrictMode, useState } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createRouter } from "@tansatack/react-router";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
